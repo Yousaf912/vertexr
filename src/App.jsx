0efa4203f15a9474ebc9dc10cc10d27434787c91
yousaf <yousafva9@gmail.com>
2025-02-28 09:48:50 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import Header from './Components/Header/Header'
-import BodySection from './Components/bodySection'
+
 import Login from './Components/Login/Login'
 
 export default function App() {
   return (
     <div>
-      {/* <Header/>
-      <BodySection/> */}
+    
       <Login/>
     </div>
   )
